d2d1f13420abc7c4e398feecd0a3bb35a3acd167
Prof-Matheus-Humenhuk <matheus.humenhuk.oliveira@escola.pr.gov.br>
2026-06-24 16:28:19 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// subsolo - interações do blog
 
+document.addEventListener("DOMContentLoaded", function () {
+  const btnEntrar = document.querySelector(".btn-entrar");
+
+  if (btnEntrar) {
+    btnEntrar.addEventListener("click", function () {
+      alert("Em breve: primeiras matérias do Subsolo.");
+    });
+  }
+});
